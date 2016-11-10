2e3825b3ff7b5b0e146636ebd15d0b874a12a66f
Jason Dudley <jasondudley@mac.com>
2016-11-10 09:48:03 -0700

    Current public with join form completed

---
+++ public/js/index.js
@@ -0,0 +1,51 @@
+//smooth scroll
+$(document).ready(function () {
+  //smooth scroll
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+$(function () {
+  const joinPg = 'components/join.html';
+  //collapse mobile menu when item selected
+  $('.nav a').on('click', function () {
+    if ($('.navbar-toggle').css('display') != 'none') {
+      $(".navbar-toggle").trigger("click");
+    }
+  });
+  //subscribe button
+  $('#newsletter-submit').on('click',function(){
+    var newEmail = $('#newsletter-email').val();
+    var result = validateEmail(newEmail);
+    if (result){
+      $.post('http://1xtactical.com/api/mailing-list', {"member":{"email": newEmail}});
+      $('#newsletter-signup')[0].reset();
+      $('#mailing-list-success').removeClass('hidden');
+    } else {
+      $('#mailing-list-danger').removeClass('hidden');
+    }
+  })
+  function validateEmail(email) 
+  {
+      var re = /\S+@\S+\.\S+/;
+      return re.test(email);
+  }
+  //join button
+  // $('.join-btn').on('click', function () {
+  //   $('.signup-box').load(joinPg);
+  //   $('#membership-btn-div').addClass('hidden');
+  //   $('.signup-box').removeClass('hidden');
+  //   onJoinLoad('loadHandlers');
+  //   setTimeout(function(){
+  //     $('.signup-box').slideDown();
+  //   },1000);
+  // });
+});
