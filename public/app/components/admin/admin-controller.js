391a2a16b6fdc08c78092bfb6a7022856608b626
runnerguy55 <michaelhoffmanj@gmail.com>
2016-11-19 21:11:31 -0700

    admin http post

--- public/app/components/admin/admin-controller.js
+++ public/app/components/admin/admin-controller.js
@@ -1,39 +1,25 @@
-(function() {
+(function () {
 
-  angular.module('1x')
+    angular.module('1x')
         .component('admin', {
             templateUrl: 'app/components/admin/admin.html',
-          controller: AdminController
-                    })
-    
+            controller: AdminController
+        })
+
     function AdminController(AdminService) {
         var ac = this;
         var as = AdminService;
 
-        ac.moreImage=()=>{ 
-            var newImg = angular.element( document.querySelector( '#images' ) );
-            newImg.append(`<input ng-model="$ctrl.newItem.image" type="text" placeholder="Image Url">`);  
-                }
-        
-        ac.addItem=(product)=>{
-           $.post('localhost:8080/api/store', {"product":{ 
-            "category": product.category, 
-           "name": product.name,
-            "description":product.description,
-            "specs": product.specs || '',
-            "images": product.images || [],
-            "msrp": product.msrp,
-            "memberPrice": product.memberPrice,
-            "nonMemberPrice": product.nonMemberPrice,
-             "inStock": true,
-             "inStore": true
-              }})
-
+        ac.moreImage = () => {
+            var newImg = angular.element(document.querySelector('#images'));
+            newImg.append(`<input ng-model="$ctrl.newItem.image" type="text" placeholder="Image Url">`);
         }
-    
+
+        ac.addItem = (product) => {
+            as.addProduct(product)
+        }
+    }
 
 
-}
-        
 
 })(); 
